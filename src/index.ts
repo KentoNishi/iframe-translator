31b6a4d3fdd1321295b696b2ae582a3dbb6af467
Kento Nishi <kento24gs@outlook.com>
2022-01-13 19:52:00 -0800

    some processing improvements

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,39 @@
 import { TranslateRequest } from './types';
 
 let targetLanguage = 'en';
+let wrapper: HTMLDivElement | null = null;
 
 (window as any).googleTranslateElementInit = () => {
   setInterval(() => {
     document.body.scrollTop = document.body.scrollHeight * 2;
-    const e: HTMLInputElement | null = document.querySelector('.goog-te-combo');
-    if (e) {
-      e.value = targetLanguage;
-      e.dispatchEvent(new Event('change'));
-    }
   }, 1000);
+  wrapper = document.createElement('div');
+  wrapper.id = 'parent-wrapper';
+  document.body.appendChild(wrapper);
 };
 
 const messageCallback = (payload: {
   data: string;
 }, calledViaTranslate=false) => {
+  if (!wrapper) return;
   const e = document.createElement('div');
   const data: TranslateRequest = JSON.parse(payload.data);
   e.innerText = data.text;
   const randomID = data.messageID.replace(/[^a-zA-Z0-9]/g, '');
   e.id = randomID;
-  document.body.appendChild(e);
-  (window as any).google.translate.TranslateElement({}, e.id);
+  wrapper.appendChild(e);
   const destroy = () => {
     mutationObserver.disconnect();
     e.outerHTML = '';
     document.querySelectorAll(`#${randomID}`)?.forEach(e => e.remove());
   };
+  setTimeout(() => {
+    const e: HTMLInputElement | null = document.querySelector('.goog-te-combo');
+    if (e) {
+      e.value = targetLanguage;
+      e.dispatchEvent(new Event('change'));
+    }
+  }, 100);
   const mutationObserver = new MutationObserver(() => {
     const textElem = e.querySelector('font');
     if (textElem && textElem.textContent !== data.text) {
@@ -38,16 +44,20 @@ const messageCallback = (payload: {
         type: 'response',
         messageID: data.messageID,
       };
-      console.debug(response);
       if (!calledViaTranslate) {
         window.parent.postMessage(JSON.stringify(response), '*');
       }
       destroy();
+      clearTimeout(eliminator);
     }
   });
+  const eliminator = setTimeout(destroy, 5000);
   mutationObserver.observe(e, {
     attributes: true, childList: true, characterData: true
   });
+  setTimeout(() => {
+    (window as any).google.translate.TranslateElement({}, e.id);
+  }, 0);
 };
 
 window.addEventListener('message', messageCallback);
