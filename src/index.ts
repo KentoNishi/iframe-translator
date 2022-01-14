12b7bd828b8306cc4f56d275b71aba0005ee2a36
Kento Nishi <kento24gs@outlook.com>
2022-01-13 16:20:55 -0800

    some small improvements

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,50 @@
 import { TranslateRequest } from './types';
 
+let targetLanguage = 'en';
+
 (window as any).googleTranslateElementInit = () => {
-  setTimeout(() => {
+  setInterval(() => {
+    document.body.scrollTop = document.body.scrollHeight;
     const e: HTMLInputElement | null = document.querySelector('.goog-te-combo');
     if (e) {
-      e.value = 'en';
+      e.value = targetLanguage;
       e.dispatchEvent(new Event('change'));
     }
   }, 1000);
-  setInterval(() => {
-    document.body.scrollTop = document.body.scrollHeight;
-  }, 0);
 };
 
-window.addEventListener('message', (payload) => {
+const messageCallback = (payload: {
+  data: string;
+}, calledViaTranslate=false) => {
   const e = document.createElement('div');
   const data: TranslateRequest = JSON.parse(payload.data);
   e.innerText = data.text;
-  e.id = Date.now().toString();
+  e.id = data.messageID;
   document.body.appendChild(e);
   (window as any).google.translate.TranslateElement({}, e.id);
   const mutationObserver = new MutationObserver(() => {
     const textElem = e.querySelector('font');
     if (textElem && textElem.textContent !== data.text) {
+      targetLanguage = data.targetLanguage;
       const response: TranslateRequest = {
-        targetLanguage: data.targetLanguage,
+        targetLanguage,
         text: textElem.textContent,
-        type: 'response'
+        type: 'response',
+        messageID: data.messageID,
       };
       console.debug(response);
-      window.parent.postMessage(JSON.stringify(response), '*');
+      if (!calledViaTranslate) {
+        window.parent.postMessage(JSON.stringify(response), '*');
+      }
+      document.querySelectorAll(`#${data.messageID}`).forEach(e => e.remove());
+      mutationObserver.disconnect();
+      e.remove();
     }
   });
   mutationObserver.observe(e, {
     attributes: true, childList: true, characterData: true
   });
-});
+};
+
+window.addEventListener('message', messageCallback);
+(window as any).translate = (data: string) => messageCallback({ data }, true);
--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,5 @@ export interface TranslateRequest {
   type: 'request' | 'response';
   targetLanguage: string;
   text: string;
+  messageID: string;
 }
