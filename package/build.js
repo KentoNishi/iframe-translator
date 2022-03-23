2e6d5c69b12be9e1fe350d6ab45747e5f0e9d1f7
Kento Nishi <kento24gs@outlook.com>
2022-03-22 22:35:03 -0700

    fixed build scripts

--- package/build.js
+++ package/build.js
@@ -1,11 +1,20 @@
 import {
-  copyFileSync
+  copyFileSync,
+  mkdirSync,
+  existsSync
 } from 'fs';
+import { execSync } from 'child_process';
+
+if (!existsSync('dist')) {
+  mkdirSync('dist');
+}
+
+execSync('tsc --target es6 --declaration --outDir ./dist index.ts');
 
 [
-  'package.json',
-  '../README.md',
-  '../LICENSE',
+  ['package.json', 'package.json'],
+  ['../README.md', 'README.md'],
+  ['../LICENSE', 'LICENSE'],
 ].forEach(file => {
-  copyFileSync(file, `dist/${file}`);
+  copyFileSync(file[0], `dist/${file[1]}`);
 });
